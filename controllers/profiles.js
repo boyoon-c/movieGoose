1a2a5da80614478e336e778ed8d38efd1f1546f6
boyoon-c <boyoonchang@gmail.com>
2021-07-25 22:55:02 -0700

    Add view users page

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -2,7 +2,26 @@ import { Profile } from '../models/profile.js'
 import { Movie } from '../models/movie.js'
 
 export {
-    index
+    index,
+    show
+}
+
+function show(req,res){
+    Profile.findById(req.params.id)
+    .then(profile =>{
+        Profile.findById(req.user.profile)
+        .then(userProfile =>{
+            res.render("profile/show", {
+                title: `${profile.name}'s profile`,
+                profile,
+                userProfile
+            })
+        })
+    })
+    .catch(err =>{
+        console.log(err)
+        res.redirect("/")
+    })
 }
 
 function index(req, res) {
--- routes/profiles.js
+++ routes/profiles.js
@@ -8,6 +8,7 @@ export{
 const router = Router()
 
 router.get('/', isLoggedIn, profilesCtrl.index)
+router.get('/:id', isLoggedIn, profilesCtrl.show)
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
